18ce27ae5523972132d0330f9da4b424583918fa
ShellyO09 <83631392+ShellyO09@users.noreply.github.com>
2021-08-15 09:46:00 +1000

    Update main2.js

--- assesment/exercise2/main2.js
+++ assesment/exercise2/main2.js
@@ -8,20 +8,25 @@ document.addEventListener('DOMContentLoaded', function(){
             var req = new XMLHttpRequest(); // XMLHttpRequest-- create object
             req.onreadystatechange = function() { // XMLHttpRequest--Event handler called when the state of an object changes
             if(req.readyState == 4 && req.status == 200){ // When the response from the server is completed or successed
-    
             var data = JSON.parse(req.responseText); // Put JSON file data into variable 
             var len = data.length; // Get JSON data length
-    
+            var window_size=window.innerWidth;
+            
             for(var i=0; i<len; i++) {
+                let title=(data[i].title);
+                let content=(data[i].content);
                 var li = document.createElement('li');
-                li.textContent = data[i].title;
-                document.getElementById('list').appendChild(li);
-                li.classList.add("tab");
-    
                 var div = document.createElement('div');
-                div.textContent = data[i].content;
-                document.getElementById('list2').appendChild(div);
+                li.textContent = title;                
+                div.textContent = content;
+                li.classList.add("tab");
                 div.classList.add("panel");
+                document.getElementById('list').appendChild(li);
+                if(window_size<=600){
+                    document.getElementById('list2').appendChild(li);
+                }
+                    document.getElementById('list2').appendChild(div);
+                
                 if(i==0){
                     li.classList.add("is-active");
                     div.classList.add("is-show");
@@ -42,12 +47,30 @@ document.addEventListener('DOMContentLoaded', function(){
             // when the tab clicked
             function tabSwitch(){
                 // change class value
-                document.getElementsByClassName('is-active')[0].classList.remove('is-active');
-                this.classList.add('is-active');
-                // change class value
-                document.getElementsByClassName('is-show')[0].classList.remove('is-show');
                 const arrayTabs = Array.prototype.slice.call(tabs);
                 const index = arrayTabs.indexOf(this);
-                document.getElementsByClassName('panel')[index].classList.add('is-show');
+                if(this.classList.contains('is-active') == true){
+                    this.classList.remove('is-active');
+                    document.getElementsByClassName('panel')[index].classList.remove('is-show');
+                }
+                else{
+                    this.classList.add('is-active');
+                    document.getElementsByClassName('panel')[index].classList.add('is-show');
+                }
+                
+                //Check if there is any other tab is open
+                for(let i = 0; i < tabs.length; i++) {
+                    var tabActive=(tabs[i].classList.contains('is-active')) == true;
+                        if(tabActive && i!=index){
+                         tabs[i].classList.remove('is-active');
+                         document.getElementsByClassName('panel')[i].classList.remove('is-show');
+                        }
+                        else{
+                            console.log("not open")
+                        }
+                    }
+
             };
+            
         });
+       
